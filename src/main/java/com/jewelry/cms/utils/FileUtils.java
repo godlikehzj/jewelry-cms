c3c3056c7d58cfdefb89ae0a7a73c4c04b2507b8
godlikehzj <godlikehzj@qq.com>
2018-07-15 22:44:44 +0800

    修改路径

--- src/main/java/com/jewelry/cms/utils/FileUtils.java
+++ src/main/java/com/jewelry/cms/utils/FileUtils.java
@@ -1,12 +1,17 @@
 package com.jewelry.cms.utils;
 
 public class FileUtils {
-    public static final String img_path = "/files/img/";
+//    public static final String img_path = "/files/img/";
+    public static final String img_path = "\\files\\img\\";
+
     public static final String video_path = "/files/video/";
 
-    public static final String upload_img_path = "/opt/www/files/img/";
+//    public static final String upload_img_path = "/opt/www/files/img/";
+    public static final String upload_img_path = "e:\\www\\files\\img\\";
+
     public static final String upload_video_path = "/opt/www/files/video/";
-    public static final String url = "http://120.27.15.2";
+//    public static final String url = "http://120.27.15.2:8080";
+    public static final String url = "http://47.93.40.166:8080";
 
     public static final String img_url = url + img_path;
     public static final String video_url = url + video_path;
